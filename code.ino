a5813d2b40cef0e45406db52cde545896e9665a2
Mitisha Agarwal <58738510+mitishaa@users.noreply.github.com>
2020-08-09 12:32:24 +0530

    updated code.ino

--- code.ino
+++ code.ino
@@ -15,7 +15,13 @@ void setup() {
   {
     Serial.print(".");
   }
-
+  
+  sendData("AT+RST\r\n",2000,DEBUG);            // command to reset the module
+  sendData("AT+CWMODE=2\r\n",1000,DEBUG);       // This will configure the mode as access point
+  sendData("AT+CIFSR\r\n",1000,DEBUG);          // This command will get the ip address
+  sendData("AT+CIPMUX=1\r\n",1000,DEBUG);       // This will configure the esp for multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // This command will turn on the server on port 80
+    
   Serial.println("Connected to:");
   Serial.print(WiFi.localIP());
 }
@@ -30,7 +36,27 @@ void loop() {
   delayMicroseconds(5);
   digitalWrite(D1, LOW);
   duration = pulseln(D2, HIGH);
-  distance = duration * 0.034 / 2;                                        // Speed of sound wave divided by 2 (go and back)
+  distance = duration * 0.034 / 2;     // Speed of sound wave divided by 2 (go and back)
+    
+  if(esp8266.available())
+  {    
+    if(esp8266.find("+IPD,"))
+    {
+     delay(1000);
+     int connectionId = esp8266.read()-48; 
+     String webpage = "<h1>IOT Garbage Monitoring System</h1>";
+       webpage += "<p><h2>";   
+       if (distance<5)
+       {
+        webpage+= " Trash can is Full";
+        }
+        else{
+          webpage+= " Trash can is Empty";
+          }
+       webpage += "</h2></p></body>"; 
+        
+     sendData(closeCommand,3000,DEBUG);
+
   // Displays the distance on the Serial Monitor
   Serial.print("Distance: ");
   Serial.print(distance);
@@ -38,4 +64,24 @@ void loop() {
   Serial.println();
   delay(100);
 
+}
+      
+ String sendData(String command, const int timeout, boolean debug)
+{
+    String response = "";   
+    esp8266.print(command); 
+    long int time = millis();
+    while( (time+timeout) > millis())
+    {
+      while(esp8266.available())
+      {
+        char c = esp8266.read(); 
+        response+=c;
+      }  
+    }
+    if(debug)
+    {
+      Serial.print(response);
+    }
+    return response;
 }
